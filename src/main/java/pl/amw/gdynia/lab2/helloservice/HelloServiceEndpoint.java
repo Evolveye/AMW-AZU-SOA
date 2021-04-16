3253940dbebcb2745ecd02c0d5a412f7d9381fdd
Evolveye <stolarski.paw@gmail.com>
2021-04-16 15:34:30 +0200

    Working empty response

--- src/main/java/pl/amw/gdynia/lab2/helloservice/HelloServiceEndpoint.java
+++ src/main/java/pl/amw/gdynia/lab2/helloservice/HelloServiceEndpoint.java
@@ -1,11 +1,15 @@
 package pl.amw.gdynia.lab2.helloservice;
 
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
 public class HelloServiceEndpoint {
+    private static final String NAMESPACE_URI = "http://helloservice.lab2.gdynia.amw.pl";
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "sayHiReq")
     @ResponsePayload
     public SayHiRes sayHi(@RequestPayload SayHiReq sayHiReq) {
         var res = new SayHiRes();
