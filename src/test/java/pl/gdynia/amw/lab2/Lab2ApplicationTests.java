2d7485ad4cda86f2628e541c41f5cb23cdadcf0a
DanielSoltysiak <72917460+DanielSoltysiak@users.noreply.github.com>
2021-04-16 17:24:42 +0200

    Initial Commit

---
+++ src/test/java/pl/gdynia/amw/lab2/Lab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gdynia.amw.lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
